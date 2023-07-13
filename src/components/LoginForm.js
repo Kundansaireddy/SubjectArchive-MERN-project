959167459911cdff4c1d7a0747366441172a7673
Kundan Sai Reddy <81676583+Kundansaireddy@users.noreply.github.com>
2023-07-13 23:23:58 +0530

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -24,7 +24,7 @@ const LoginForm = () => {
     setIsLoading(true);
     try {
       const response = await fetch(
-        "https://authenticate-api-9vg2.onrender.com/api/checkUser",
+        "https://calm-erin-dove-sock.cyclic.app/api/checkUser",
         {
           method: "POST",
           headers: {
